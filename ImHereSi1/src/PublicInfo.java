55490f1f2541d9ed0695d043f1d8a2bf5d404081
raquelrag <raquelrag@e0a8c252-900a-11de-bb70-8db92513eca4>
2009-09-07 17:20:53 +0000

    Pequenas mudanças em User e PublicInfo, apenas Gets e Sets

--- ImHereSi1/src/PublicInfo.java
+++ ImHereSi1/src/PublicInfo.java
@@ -4,5 +4,28 @@ public class PublicInfo {
 	private String name;
 	private String login;
 	private Position position;
+	private boolean visible;
 	
+	public PublicInfo(String name, String login, Position position, boolean visible){
+		this.name = name;
+		this.login = login;
+		this.position = position;
+		this.visible = visible;
+	}
+	
+	public boolean isVisible(){
+		return this.visible;
+	}
+	
+	public Position getPosition(){
+		return this.position;
+	}
+	
+	public String getName(){
+		return this.name;
+	}
+	
+	public void setName(String newName){
+		this.name = newName;
+	}
 }
