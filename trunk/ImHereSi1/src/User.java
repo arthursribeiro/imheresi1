c3a8d959d30f99f34dffcab3b5d2de43eeab8384
raquelrag <raquelrag@e0a8c252-900a-11de-bb70-8db92513eca4>
2009-09-07 17:20:53 +0000

    Pequenas mudanças em User e PublicInfo, apenas Gets e Sets

--- trunk/ImHereSi1/src/User.java
+++ trunk/ImHereSi1/src/User.java
@@ -5,5 +5,37 @@ public class User {
 
 	private Collection<PublicInfo> friends;
 	private String password;
+	private Position myPosition;
+	private String email;
+	private String telephoneNumber;
+	private PublicInfo myPublicInfo;
+	
+	public User(){
+		
+	}
+	
+	public void updatePosition(int ip){
+//		this.myPosition.setPosition();
+	}
+	
+	public void updateName(String newName){
+		this.myPublicInfo.setName(newName);
+	}
+	
+	public void updateTelephoneNumber(String newTelephoneNumber){
+		this.telephoneNumber = newTelephoneNumber;
+	}
+	
+	public void updateEmail(String newEmail){
+		this.email = newEmail;
+	}
+	
+	public void updatePassword(String newPassword){
+		this.password = newPassword;
+	}
+	
+	public void addFriend(PublicInfo friend){
+		this.friends.add(friend);
+	}
 	
 }
