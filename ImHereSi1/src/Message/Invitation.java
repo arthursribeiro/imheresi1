b65ae854ce8eefce125a960733587dba97dd8405
raquelrag <raquelrag@e0a8c252-900a-11de-bb70-8db92513eca4>
2009-09-15 20:48:24 +0000

    Facade pro Us4, acho que passa. Não consegui testar de um jeito 100% =/

---
+++ ImHereSi1/src/Message/Invitation.java
@@ -0,0 +1,41 @@
+package Message;
+
+public class Invitation implements Message{
+
+	private String fromName;
+	private String to;
+	private String fromMail;
+	
+	public Invitation(String fromName, String fromMail, String to){
+		this.fromName = fromName;
+		this.to = to;
+		this.fromMail = fromMail;
+	}
+
+	public String build() {
+		StringBuilder sB = new StringBuilder();
+		sB.append("From: iam@email.com");
+		sB.append(System.getProperty("line.separator"));
+		sB.append("to: "+this.to);
+		sB.append(System.getProperty("line.separator"));
+		sB.append("Subject: "+this.fromName+" gostaria de compartilhar sua localização com você");
+		sB.append(System.getProperty("line.separator"));
+		sB.append(System.getProperty("line.separator"));
+		sB.append(this.fromName+" ("+this.fromMail+") ");
+		sB.append("gostaria de compartilhar sua localização com você usando o I'mHere. Você também pode fazer o mesmo com os seus amigos, para mais informações acesse http://iamhere.com.");
+		sB.append("Para aceitar ou recusar, clique no link abaixo.");
+		sB.append(System.getProperty("line.separator"));
+		sB.append(System.getProperty("line.separator"));
+		sB.append("http://iamhere.com/compartilhar");
+		sB.append(System.getProperty("line.separator"));
+		sB.append(System.getProperty("line.separator"));
+		sB.append("***************************************************************************************************");
+		sB.append(System.getProperty("line.separator"));
+		return sB.toString();
+	}
+
+	public String getPath() {
+		return "files/outputs/convites.log";
+	}
+
+}
