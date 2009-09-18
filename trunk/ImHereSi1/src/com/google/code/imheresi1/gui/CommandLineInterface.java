b5a575ef46300ffb6acdaa914633222f02d79fe4
laertexavier <laertexavier@e0a8c252-900a-11de-bb70-8db92513eca4>
2009-09-18 04:33:18 +0000

    oi ola

--- trunk/ImHereSi1/src/com/google/code/imheresi1/gui/CommandLineInterface.java
+++ trunk/ImHereSi1/src/com/google/code/imheresi1/gui/CommandLineInterface.java
@@ -6,38 +6,58 @@ public class CommandLineInterface {
 
 	static Scanner input;
 	static final String SEPARATOR = System.getProperty("line.separator");
-	static final String OPT = "Choose your option: " + SEPARATOR + "1. Login"
-			+ SEPARATOR + "2. hIHI" + SEPARATOR + "3. Exit";
-	static final int LOG = 1;
-	static final int SEILAH = 2;
+
+
+	static final String PROMPT_1 = "Bem vindo ao I'm her!" + SEPARATOR +  SEPARATOR + 
+	"Escolha a opcao desejada:" + SEPARATOR + "1. Login" 	+ SEPARATOR + "2. hIHI" + SEPARATOR + "3. Exit";
+	static final String PROMPT_2 = "Opcao: ";
+
+
+	static final int LOGIN = 1;
+	static final int CREATE_USER = 2;
 	static final int SAIR = 3;
+
+	private static int getOption(String option){
+		int chosenNumber;
+
+		try{
+			chosenNumber = Integer.parseInt(option);
+		} catch (NumberFormatException e) {
+			chosenNumber = -1;
+		}
+
+		return chosenNumber;
+	}
+
+	private static void createUser() {
+
+
+	}
 
 	public static void main(String[] args) {
 		input = new Scanner(System.in);
-		initialMenu();
-	}
 
-	static void initialMenu() {
-		System.out.println(OPT);
-		int opt = input.nextInt();
-		while (opt != SAIR) {
-			switch (opt) {
-			case LOG:
+
+		System.out.println(PROMPT_1);
+		int option = getOption(input.nextLine());
+
+		while (option != SAIR) {
+			switch (option) {
+			case LOGIN:
 				System.out.println("oi");
 				break;
-			case SEILAH:
-				System.out.println("danou-se");
+			case CREATE_USER:
+				createUser();
 				break;
 			default:
-				System.out.println("oxe");
-				break;
+				System.out.println(SEPARATOR + "<<< Opcao invalida! >>>");
+			break;
 			}
-			System.out.println(OPT);
-			opt = input.nextInt();
-		}
-	}
 
-	static void laerteHelpMe() {
+			System.out.println(SEPARATOR + PROMPT_1);
+			option = getOption(input.nextLine());
+
+		}
 
 	}
 }
